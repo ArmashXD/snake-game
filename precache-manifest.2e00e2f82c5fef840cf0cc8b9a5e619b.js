ac9b5ac3b77ebd95512413c2bcfb3618034477f2
unknown <armashash@gmail.com>
2020-08-19 10:21:26 +0500

    Updates

---
+++ precache-manifest.2e00e2f82c5fef840cf0cc8b9a5e619b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f089062a3edf278c0c506af1dc2713ec",
+    "url": "/ArmashXD/snake-game/index.html"
+  },
+  {
+    "revision": "b4c91c7ea763f5b3148a",
+    "url": "/ArmashXD/snake-game/static/css/main.bece389a.chunk.css"
+  },
+  {
+    "revision": "888361100b2194d71988",
+    "url": "/ArmashXD/snake-game/static/js/2.6f476254.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ArmashXD/snake-game/static/js/2.6f476254.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4c91c7ea763f5b3148a",
+    "url": "/ArmashXD/snake-game/static/js/main.1e018dad.chunk.js"
+  },
+  {
+    "revision": "1d28d074b06beffaf76b",
+    "url": "/ArmashXD/snake-game/static/js/runtime-main.ac4de636.js"
+  }
+]);
